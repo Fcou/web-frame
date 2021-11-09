f8777f481bf03f61d1d472d59f40a178c2a1c706
Fcou <lyz672673107@gmail.com>
2021-11-09 14:42:58 +0800

    fcou

--- app/http/route.go
+++ app/http/route.go
@@ -3,11 +3,15 @@ package http
 import (
 	"github.com/Fcou/web-frame/app/http/module/demo"
 	"github.com/Fcou/web-frame/framework/gin"
+	"github.com/Fcou/web-frame/framework/middleware/static"
 )
 
+// Routes 绑定业务层路由
 func Routes(r *gin.Engine) {
 
-	r.Static("/dist/", "./dist/")
+	// /路径先去./dist目录下查找文件是否存在，找到使用文件服务提供服务
+	r.Use(static.Serve("/", static.LocalFile("./fcou/dist", false)))
 
+	// 动态路由定义
 	demo.Register(r)
 }
